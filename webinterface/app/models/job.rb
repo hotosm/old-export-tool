6644ebd293c68d80cb0ee15af6424786bd03e168
Harry Wood <github@onlineanimals.co.uk>
2014-01-04 21:18:40 +0000

    constant in job model allow up to 200 sized jobs
    the max enforced by the job_controller form will be either 100 or 200 depending on 'current_user.admin' state, so here in the job model we allow up to 200.

--- webinterface/app/models/job.rb
+++ webinterface/app/models/job.rb
@@ -51,8 +51,10 @@ class Job < ActiveRecord::Base
 
 
    # maximum of (latmax-latmin) * (lonmax-lonmin)
-   BBOX_MAX = 100
-
+   # For normal users max is 100, for admins 200, enforced by the form controller
+   # for purposes of the jobs model validation, the max is 200
+   BBOX_MAX = 200
+   
    # pagination count per page
    PPAGE = 200
    
